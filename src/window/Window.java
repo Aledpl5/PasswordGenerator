967f018887f7bb94a2bce9209394a90747c55630
Baddy2904 <battiatomatteo29@gmail.com>
2023-09-27 16:33:35 +0200

    window

--- src/window/Window.java
+++ src/window/Window.java
@@ -1,13 +1,57 @@
 package window;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class Window extends JFrame{
 
-    private final int WIDTH = 500, HEIGHT = 350;
+public class Window extends JFrame implements ActionListener{
+
+    JPanel p;
+    private final int WIDTH = 850, HEIGHT = 500;
+    JLabel lbl;
+    String numc[]={" ","16", "32", "64"};  
+    JComboBox cb;
+    JButton btn;
 
     public Window(){
+        this.p = new JPanel(null);
+        
+        setJLabel();
+        setComboBox();
+        setButton();
+
+        addElement();
+        setPos();
+
+        btn.addActionListener(this);
+
         setFrame();
+    }
+
+    private void setButton() {
+        btn= new JButton("Genera");
+    }
+
+    private void setComboBox() {
+        cb = new JComboBox(numc);  
+        cb.setSelectedIndex(0);
+        
+    }
+
+    private void addElement() {
+        this.p.add(lbl);
+        this.p.add(cb);
+        this.p.add(btn);
+    }
+
+    private void setPos() {
+        lbl.setBounds(5,20, 5, 20);
+        cb.setBounds(10,20, 5, 5);
+        btn.setBounds(30,20, 5, 5);
+    }
+
+    private void setJLabel() {
+        lbl = new JLabel("Seleziona la grandezza della password: ");
     }
 
     /**
@@ -18,6 +62,12 @@ public class Window extends JFrame{
         this.setResizable(false);
         this.setSize(WIDTH, HEIGHT);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("GENERATORE PASSWORD");
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    } 
 }
